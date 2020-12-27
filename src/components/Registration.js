9d5825b4353e2d3ee5684b12fe80a045e8f552f2
akirayamada-haha <hondamurai99@proton.me>
2020-12-26 22:19:38 -0600

    registration and signIn front end functionality

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,7 +1,108 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import AuthApiService from "../services/auth-api-service";
+import TokenService from "../services/token-service.js";
 
 export class Registration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      params: {
+        signUpEmail: "",
+        signUpPassword: "",
+      },
+    };
+  }
+
+  formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(
+      (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+    );
+    return queryItems.join("&");
+  }
+
+  validateEmail(inputEmail) {
+    let outputEmail = inputEmail;
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!inputEmail.match(mailformat)) {
+      outputEmail = "";
+    }
+    return outputEmail;
+  }
+
+  validatePassword(inputPassword) {
+    let outputPassword = inputPassword;
+    // at least one number, one lowercase and one uppercase letter
+    // at least eight characters that are letters, numbers or the underscore
+    let passwordformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/;
+    if (!inputPassword.match(passwordformat)) {
+      outputPassword = "";
+    }
+    return outputPassword;
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //create an object to store the search filters
+    const data = {};
+
+    //get all the from data from the form component
+    const formData = new FormData(e.target);
+
+    //for each of the keys in form data populate it with form value
+    for (let value of formData) {
+      data[value[0]] = value[1];
+    }
+    console.log(data);
+    let { signUpEmail, signUpPassword, verifyPassword } = data;
+    if (this.validateEmail(signUpEmail) === "") {
+      this.setState({
+        error: "email is not valid",
+      });
+    }
+    if (this.validatePassword(signUpPassword) === "") {
+      this.setState({
+        error: "password is not valid",
+      });
+    }
+    if (signUpPassword != verifyPassword) {
+      this.setState({
+        error: "passwords do not match",
+      });
+    }
+    //assigning the object from the form data to params in the state
+    this.setState({
+      params: data,
+    });
+
+    //check if the state is populated with the search params data
+    console.log(this.state.params);
+
+    AuthApiService.postUser({
+      email: signUpEmail,
+      password: signUpPassword,
+    })
+
+      .then((response) => {
+        console.log("user:", response);
+        TokenService.saveAuthToken(response.authToken);
+        TokenService.saveUserId(response.id);
+        window.location = "/library";
+      })
+
+      .catch((res) => {
+        this.setState({ error: res.error });
+      });
+  };
+
   render() {
+    //if there is an error message display it
+    const errorMessage = this.state.error ? (
+      <p className="error-message">{this.state.error}</p>
+    ) : (
+      false
+    );
     return (
       <div>
         <section className="registration">
@@ -20,45 +121,43 @@ export class Registration extends Component {
             Registration Failed!! Please see System Adminstrator!!
           </div>
 
-          <form className="registration-form">
+          <form className="registration-form" onSubmit={this.handleSubmit}>
             <h1>Travel Bucket Registration</h1>
-            <div className="form-item">
-              <label htmlFor="register-email">Email</label>
+            <fieldset className="welcome-fieldset">
+              <div className="create-user-message">
+                <h3>Sign Up for an Account </h3>
+                {errorMessage}
+              </div>
+              <label htmlFor="signUpEmail">Enter Email Address: </label>
               <input
+                name="signUpEmail"
                 type="text"
-                placeholder="email"
-                required=""
-                id="register-email"
+                placeholder="Email"
+                required
               />
-            </div>
-            <div className="form-item">
-              <label htmlFor="register-password">Password</label>
+              <label htmlFor="signUpPassword">Enter Password: </label>
               <input
-                type="text"
-                placeholder="password"
-                required=""
-                id="register-password"
+                name="signUpPassword"
+                type="password"
+                placeholder="Password"
+                required
               />
-            </div>
-            <div className="form-item">
-              <label htmlFor="register-confirm-password">
-                Confirm Password
-              </label>
+              <label htmlFor="verifyPassword">Re-enter Password: </label>
               <input
-                type="text"
-                placeholder="confirm-password"
-                required=""
-                id="register-confirm-password"
+                name="verifyPassword"
+                type="password"
+                placeholder="Re-enter Password"
+                required
               />
-            </div>
-            <div className="form-item">
-              <a href="#" className="myButton">
-                Register
-              </a>
-              <p>
-                Already have an account? <a href="#">Sign In</a>
-              </p>
-            </div>
+              {/* <label htmlFor="getStarted">Let's Get Started!</label> */}
+              <div className="form-item">
+                <input name="getStarted" type="submit" value="Register" className="myButton"/>
+                <p>
+                  Already have an account? <NavLink to="/sign-in" className="new-user-cta">Sign In Here...</NavLink>
+                </p>
+                {/* <a href="placeholder">Returning User? Log In Here...</a> */}
+              </div>
+            </fieldset>
           </form>
         </section>
       </div>
