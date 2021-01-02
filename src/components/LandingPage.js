f6ecfad5f4ea2d331a91d815ae73728356fb37fd
akirayamada-haha <hondamurai99@proton.me>
2021-01-01 20:10:13 -0600

    Landing Page Functional and cleaned, needs privacy route in backend

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,100 +1,283 @@
 import React, { Component } from "react";
+import config from "../config";
+import { Link } from "react-router-dom";
 
 export class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      publicItems: [],
+    };
+  }
+
+  componentDidMount() {
+    let getpublicItemsUrl = `${config.API_ENDPOINT}/items`;
+
+    fetch(getpublicItemsUrl)
+      .then((itemsInList) => itemsInList.json())
+      .then((itemsInList) => {
+        console.log(itemsInList);
+        this.setState({
+          publicItems: itemsInList,
+        });
+        console.log(this.state);
+      })
+
+      .catch((error) => this.setState({ error }));
+  }
   render() {
+
+    let showPublicItem = "";
+
+    if (this.state.publicItems.length == 0) {
+      showPublicItem = <p>No Items</p>;
+    } else {
+      showPublicItem = this.state.publicItems.map((item, key) => {
+        let iFrameUrl = `https://maps.google.com/maps?q=${item.keyword}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+        let editItemUrl = `/edit-item-page/${item.id}`;
+        let ratingOutput = "";
+        if (item.rating == 1) {
+          ratingOutput = (
+            <li className="item-rating">
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+            </li>
+          );
+        } else if (item.rating == 2) {
+          ratingOutput = (
+            <li className="item-rating">
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+            </li>
+          );
+        } else if (item.rating == 3) {
+          ratingOutput = (
+            <li className="item-rating">
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+            </li>
+          );
+        } else if (item.rating == 4) {
+          ratingOutput = (
+            <li className="item-rating">
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star"></i>
+            </li>
+          );
+        } else if (item.rating == 5) {
+          ratingOutput = (
+            <li className="item-rating">
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+              <i className="fas fa-star selected-icon"></i>
+            </li>
+          );
+        }
+
+        let costOutput = "";
+        if (item.cost == 1) {
+          costOutput = (
+            <li className="item-cost">
+              <i className="fas fa-dollar-sign selected-icon"></i>
+              <i className="fas fa-dollar-sign"></i>
+              <i className="fas fa-dollar-sign"></i>
+            </li>
+          );
+        } else if (item.cost == 2) {
+          costOutput = (
+            <li className="item-cost">
+              <i className="fas fa-dollar-sign selected-icon"></i>
+              <i className="fas fa-dollar-sign selected-icon"></i>
+              <i className="fas fa-dollar-sign"></i>
+            </li>
+          );
+        } else if (item.cost == 3) {
+          costOutput = (
+            <li className="item-cost">
+              <i className="fas fa-dollar-sign selected-icon"></i>
+              <i className="fas fa-dollar-sign selected-icon"></i>
+              <i className="fas fa-dollar-sign selected-icon"></i>
+            </li>
+          );
+        }
+
+        // let privacyOutput = "";
+        // if (item.is_public == 0) {
+        //   privacyOutput = <li className="is-public">Private</li>;
+        // } else {
+        //   privacyOutput = <li className="is-public">Public</li>;
+        // }
+
+        
+
+        let typeOutput = "";
+        if (item.type == "Historical") {
+          typeOutput = (
+            <li className="item-type">
+              <i className="type-icon fas fa-futbol"></i>
+              <i className="type-icon fas fa-praying-hands"></i>
+              <i className="type-icon fas fa-graduation-cap"></i>
+              <i className="type-icon fas fa-heart"></i>
+              <i className="type-icon fas fa-hiking"></i>
+              <i className="type-icon fas fa-business-time"></i>
+              <i className="type-icon fas fa-bus"></i>
+              <i className="type-icon fas fa-landmark selected-icon"></i>{" "}
+              Historical
+            </li>
+          );
+        } else if (item.type == "Romantic") {
+          typeOutput = (
+            <li className="item-type">
+              <i className="type-icon fas fa-futbol"></i>
+              <i className="type-icon fas fa-praying-hands"></i>
+              <i className="type-icon fas fa-graduation-cap"></i>
+              <i className="type-icon fas fa-hiking"></i>
+              <i className="type-icon fas fa-business-time"></i>
+              <i className="type-icon fas fa-bus"></i>
+              <i className="type-icon fas fa-landmark"></i>
+              <i className="type-icon fas fa-heart selected-icon"></i> Romantic
+            </li>
+          );
+        } else if (item.type == "Outdoor") {
+          typeOutput = (
+            <li className="item-type">
+              <i className="type-icon fas fa-futbol"></i>
+              <i className="type-icon fas fa-praying-hands"></i>
+              <i className="type-icon fas fa-graduation-cap"></i>
+              <i className="type-icon fas fa-heart"></i>
+              <i className="type-icon fas fa-business-time"></i>
+              <i className="type-icon fas fa-bus"></i>
+              <i className="type-icon fas fa-landmark"></i>
+              <i className="type-icon fas fa-hiking selected-icon"></i> Outdoor
+            </li>
+          );
+        } else if (item.type == "Business") {
+          typeOutput = (
+            <li className="item-type">
+              <i className="type-icon fas fa-futbol"></i>
+              <i className="type-icon fas fa-praying-hands"></i>
+              <i className="type-icon fas fa-graduation-cap"></i>
+              <i className="type-icon fas fa-heart"></i>
+              <i className="type-icon fas fa-hiking"></i>
+              <i className="type-icon fas fa-bus"></i>
+              <i className="type-icon fas fa-landmark"></i>
+              <i className="type-icon fas fa-business-time selected-icon"></i>{" "}
+              Business
+            </li>
+          );
+        } else if (item.type == "Tourist") {
+          typeOutput = (
+            <li className="item-type">
+              <i className="type-icon fas fa-futbol"></i>
+              <i className="type-icon fas fa-praying-hands"></i>
+              <i className="type-icon fas fa-graduation-cap"></i>
+              <i className="type-icon fas fa-heart"></i>
+              <i className="type-icon fas fa-hiking"></i>
+              <i className="type-icon fas fa-business-time"></i>
+              <i className="type-icon fas fa-landmark"></i>
+              <i className="type-icon fas fa-bus selected-icon"></i> Tourist
+            </li>
+          );
+        } else if (item.type == "Religious") {
+          typeOutput = (
+            <li className="item-type">
+              <i className="type-icon fas fa-futbol"></i>
+              <i className="type-icon fas fa-graduation-cap"></i>
+              <i className="type-icon fas fa-heart"></i>
+              <i className="type-icon fas fa-hiking"></i>
+              <i className="type-icon fas fa-business-time"></i>
+              <i className="type-icon fas fa-bus"></i>
+              <i className="type-icon fas fa-landmark"></i>
+              <i className="type-icon fas fa-praying-hands  selected-icon"></i>{" "}
+              Religious
+            </li>
+          );
+        } else if (item.type == "Sport") {
+          typeOutput = (
+            <li className="item-type">
+              <i className="type-icon fas fa-praying-hands"></i>
+              <i className="type-icon fas fa-graduation-cap"></i>
+              <i className="type-icon fas fa-heart"></i>
+              <i className="type-icon fas fa-hiking"></i>
+              <i className="type-icon fas fa-business-time"></i>
+              <i className="type-icon fas fa-bus"></i>
+              <i className="type-icon fas fa-landmark"></i>
+              <i className="type-icon fas fa-futbol  selected-icon"></i> Sport
+            </li>
+          );
+        } else if (item.type == "Educational") {
+          typeOutput = (
+            <li className="item-type">
+              <i className="type-icon fas fa-futbol"></i>
+              <i className="type-icon fas fa-praying-hands"></i>
+              <i className="type-icon fas fa-heart"></i>
+              <i className="type-icon fas fa-hiking"></i>
+              <i className="type-icon fas fa-business-time"></i>
+              <i className="type-icon fas fa-bus"></i>
+              <i className="type-icon fas fa-landmark"></i>
+              <i className="type-icon fas fa-graduation-cap  selected-icon"></i>{" "}
+              Educational
+            </li>
+          );
+        }
+
+        return (
+          <div className="item-wrapper" key={key}>
+            <div className="item-column">
+              <iframe
+                className="item-image"
+                width="100%"
+                height="350"
+                id="gmap_canvas"
+                src={iFrameUrl}
+                frameBorder="0"
+                scrolling="no"
+                alt={item.keyword}
+              ></iframe>
+            </div>
+            <div className="item-column">
+              <h3 className="item-title">{item.keyword}</h3>
+              <p className="item-notes">{item.notes}</p>
+              <ul className="item-details">
+                {ratingOutput}
+                {costOutput}
+                <li className="item-currency">{item.currency}</li>
+                {typeOutput}
+                <li className="item-language">{item.language}</li>
+                <li className="item-category">
+                  {item.category}
+                </li>
+                {/* {privacyOutput} */}
+              </ul>
+            </div>
+          </div>
+        );
+      });
+    }
+
+        
+
+
     return (
       <div>
         <section className="landing-page">
           <h1>Travel Bucket</h1>
-
-          <div className="item-wrapper">
-            <div className="item-column">
-              <img
-                src="https://m.media-amazon.com/images/I/51+vl7wfRPL._AC_.jpg"
-                className="item-image"
-                alt="image of united states map"
-              />
-            </div>
-            <div className="item-column">
-              <h3 className="item-title">United States</h3>
-              <p className="item-notes">Here are the user's notes</p>
-              <ul className="item-details">
-                <li className="item-rating">
-                  <i className="fas fa-star selected-icon"></i>
-                  <i className="fas fa-star selected-icon"></i>
-                  <i className="fas fa-star selected-icon"></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                </li>
-                <li className="item-cost">
-                  <i className="fas fa-dollar-sign selected-icon"></i>
-                  <i className="fas fa-dollar-sign selected-icon"></i>
-                  <i className="fas fa-dollar-sign"></i>
-                </li>
-                <li className="item-currency">
-                  America (United States) Dollars - USD
-                </li>
-                <li className="item-type">
-                  <i className="fas fa-futbol selected-icon"></i>
-                  <i className="fas fa-praying-hands"></i>
-                  <i className="fas fa-graduation-cap"></i>
-                  <i className="fas fa-landmark"></i>
-                  <i className="fas fa-heart"></i>
-                  <i className="fas fa-hiking"></i>
-                  <i className="fas fa-business-time"></i>
-                  <i className="fas fa-bus"></i>
-                </li>
-                <li className="item-language">English</li>
-                {/* <!-- <li className="item-category">Been There Done That!</li> --> */}
-                {/* <!-- <li className="is-public">Public</li> --> */}
-              </ul>
-            </div>
-          </div>
-
-          <div className="item-wrapper">
-            <div className="item-column">
-              <img
-                src="https://m.media-amazon.com/images/I/51+vl7wfRPL._AC_.jpg"
-                className="item-image"
-                alt="image of united states map"
-              />
-            </div>
-            <div className="item-column">
-              <h3 className="item-title">United States</h3>
-              <p className="item-notes">Here are the user's notes</p>
-              <ul className="item-details">
-                <li className="item-rating">
-                  <i className="fas fa-star selected-icon"></i>
-                  <i className="fas fa-star selected-icon"></i>
-                  <i className="fas fa-star selected-icon"></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                </li>
-                <li className="item-cost">
-                  <i className="fas fa-dollar-sign selected-icon"></i>
-                  <i className="fas fa-dollar-sign selected-icon"></i>
-                  <i className="fas fa-dollar-sign"></i>
-                </li>
-                <li className="item-currency">
-                  America (United States) Dollars - USD
-                </li>
-                <li className="item-type">
-                  <i className="fas fa-futbol selected-icon"></i>
-                  <i className="fas fa-praying-hands"></i>
-                  <i className="fas fa-graduation-cap"></i>
-                  <i className="fas fa-landmark"></i>
-                  <i className="fas fa-heart"></i>
-                  <i className="fas fa-hiking"></i>
-                  <i className="fas fa-business-time"></i>
-                  <i className="fas fa-bus"></i>
-                </li>
-                <li className="item-language">English</li>
-                {/* <!-- <li className="item-category">Been There Done That!</li> --> */}
-                {/* <!-- <li className="is-public">Public</li> --> */}
-              </ul>
-            </div>
-          </div>
-        </section>
+          {showPublicItem}
+          </section>
       </div>
     );
   }
