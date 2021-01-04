cfb0693ce69e1fb27033d045c26d8bbfcfc269c0
akirayamada-haha <hondamurai99@proton.me>
2021-01-04 16:59:39 -0600

    corrected api endpoint for heroku deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api', 
-    //API_ENDPOINT:'https://workout-planner-capstone.herokuapp.com/api',
+    // API_ENDPOINT: 'http://localhost:8000/api', 
+    API_ENDPOINT:'https://travel-diary-capstone.herokuapp.com/api',
     TOKEN_KEY: 'travel-diary-capstone',
     }
