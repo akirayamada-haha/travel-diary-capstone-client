e040f6c034a54a524f05104f87985d35804d462e
akirayamada-haha <hondamurai99@proton.me>
2021-05-28 15:22:59 -0500

    updated class to className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ export class footer extends Component {
     return (
       <div>
         <footer>
-          <section class="contact" id="contact">
-            <ul class="contact-list">
+          <section className="contact" id="contact">
+            <ul className="contact-list">
               <li>
                 <a href="mailto:evanpoe91@yahoo.com?subject=Responding%20to%20Your%20Portfolio!">
-                  <i class="fa fa-envelope"></i>
+                  <i className="fa fa-envelope"></i>
                 </a>
               </li>
               <li>
@@ -17,12 +17,12 @@ export class footer extends Component {
                   href="https://www.linkedin.com/in/evan-poe-full-stack-developer/"
                   target="_blank"
                 >
-                  <i class="fab fa-linkedin"></i>
+                  <i className="fab fa-linkedin"></i>
                 </a>
               </li>
               <li>
                 <a href="https://github.com/EvanPoe" target="_blank">
-                  <i class="fab fa-github"></i>
+                  <i className="fab fa-github"></i>
                 </a>
               </li>
             </ul>
